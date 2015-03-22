b973e2db43aa9c8ba108f44467321c2921e7a599
river06 <weich06@gmail.com>
2015-03-21 21:23:14 -0400

    queue

---
+++ BST_level_order_traversal/Test.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Test{
+	public static void main(String[] args) {
+		Solution solver = new Solution();
+		TreeNode root = new TreeNode(1);
+		root = null;
+		// root.right = new TreeNode(2);
+		// root.right.left = new TreeNode(3);
+
+		List<List<Integer>> ret = solver.levelOrder(root);
+		for(int i=0; i<ret.size(); i++) {
+			System.out.println(ret.get(i).toString() );
+		}
+	}
+}
+		
+		
