1fcdac1561f6d4d80d61b07f1738c0074d32a0c9
river06 <weich06@gmail.com>
2015-10-30 15:53:01 -0400

    decode_ways:DP

--- decode_ways/Test.java
+++ decode_ways/Test.java
@@ -2,6 +2,8 @@ import java.util.*;
 public class Test {
 	public static void main(String[] args) {
 		Solution solver = new Solution();
-
+		String str ="01";
+		int ret = solver.numDecodings(str);
+		System.out.println(ret);
 	}
 }
