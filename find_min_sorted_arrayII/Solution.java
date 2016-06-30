71dead0d030908949b04b2ab33b3c68c25aa36ee
river06 <weihc06@gmail.com>
2016-06-30 00:24:03 -0400

    find_min_sorted_arrayII [1 1] wrong answer

---
+++ find_min_sorted_arrayII/Solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int findMin(int[] nums) {
+        // this function assumes that the num is sorted by increasing order
+        return findMinHelper(nums,0, nums.length-1);
+        
+    }
+    private int findMinHelper(int[] nums, int idx1, int idx2) {
+        if(idx1==idx2) return nums[idx1];
+        if(nums[idx1] < nums[idx2]) return nums[idx1];
+        
+        int idxMid = (idx1 + idx2)/2;
+        if( nums[idxMid] == nums[idx1] && nums[idxMid] == nums[idx2]){
+            while(idx1<idxMid) {
+                idx1++;
+                if( nums[idx1] != nums[idxMid]) break;
+            }
+            while(idx2> idxMid) {
+                idx2--;
+                if( nums[idx2] != nums[idxMid]) break;
+            }
+        }
+        if( nums[idxMid] >= nums[idx1]) {
+            return findMinHelper(nums, idxMid+1, idx2);
+        } else {
+            return findMinHelper(nums, idx1, idxMid);
+        }
+    }
+}
---
+++ find_min_sorted_arrayII/Test.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Test {
+	public static void main(String[] args) {
+		Solution solver = new Solution();
+        int[] nums = {3,3,1,3};
+        int ret = solver.findMin(nums);
+        System.out.println(ret);
+	}
+}
