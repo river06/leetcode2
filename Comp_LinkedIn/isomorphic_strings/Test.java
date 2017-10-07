70b0db330e2f429ab0e61fc919d6192230fc7137
river <weihc06@gmail.com>
2017-10-07 11:46:36 -0400

    isomorphic_strings/

---
+++ Comp_LinkedIn/isomorphic_strings/Test.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Test {
+	public static void main(String[] args) {
+		Solution solver = new Solution();
+		/*String s = null;
+		String t = null;*/
+		/*String s = "foo";
+		String t = "bar";*/
+		String s = "paper";
+		String t = "title";
+
+		System.out.println(solver.isIsomorphic(s, t));
+		
+	}
+}
