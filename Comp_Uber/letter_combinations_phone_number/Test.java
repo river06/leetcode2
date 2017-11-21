12fe4a30bf8910f4c22120b6b22b5c376a66bb5d
river06 <weihc06@gmail.com>
2017-11-21 15:30:28 -0800

    maybe we can use a queue

---
+++ Comp_Uber/letter_combinations_phone_number/Test.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Test {
+	public static void main(String[] args) {
+		Solution solver = new Solution();
+		String digits = "23";
+		List<String> ret = solver.letterCombinations(digits);
+		System.out.println(ret);
+	}
+}
