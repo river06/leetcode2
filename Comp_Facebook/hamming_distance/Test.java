6b0ac493d563d95df5ac44cbe676b629995073ab
river <weihc06@gmail.com>
2017-12-18 22:39:44 -0500

    hamming_distance/

---
+++ Comp_Facebook/hamming_distance/Test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Test {
+	public static void main(String[] args) {
+		Solution solver = new Solution();
+		int x = 1;
+		int y = 4;
+		int ret = solver.hammingDistance(x, y);
+
+		System.out.println(ret);
+	}
+}
