e9c1ff054cb832393c120e0c0584a96c72829047
river <weihc06@gmail.com>
2017-12-19 19:42:40 -0500

    read_N_given_Read4/

---
+++ Comp_Facebook/read_N_given_Read4/Solution.java
@@ -0,0 +1,33 @@
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
+public class Solution extends Reader4 {
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        char[] buffer = new char[4];
+		boolean eof = false;
+		int readBytes = 0;
+
+		while (readBytes < n && !eof) {
+			int read4Bytes = read4(buffer);
+
+			if (read4Bytes != 4) {
+				eof = true;
+			}
+
+			int length = Math.min(n - readBytes, read4Bytes);
+
+			for (int i=0; i<length; i++) {
+				buf[readBytes + i] = buffer[i];
+			}
+
+			readBytes += length;
+		}
+
+		return readBytes;
+    }
+}
