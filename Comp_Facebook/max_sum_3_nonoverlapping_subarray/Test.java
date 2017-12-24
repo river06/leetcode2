a07070d9085b4467dedec66dd15e233f9f2bf913
river <weihc06@gmail.com>
2017-12-24 17:06:24 -0500

    max_sum_3_nonoverlapping_subarray/

---
+++ Comp_Facebook/max_sum_3_nonoverlapping_subarray/Test.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Test {
+	public static void main(String[] args) {
+		//int[] nums = {1,2,1,2,6,7,5,1};
+		//int k = 2;
+
+		int[] nums = {7,13,20,19,19,2,10,1,1,19};
+		int k = 3;
+		Solution solver = new Solution();
+		int[] ret = solver.maxSumOfThreeSubarrays(nums, k);
+		System.out.println(Arrays.toString(ret));
+	}
+}
