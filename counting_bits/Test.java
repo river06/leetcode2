39b887921eeaff0fb200980dbea48317877330fb
river <weihc06@gmail.com>
2017-12-29 23:26:39 -0600

    counting_bits/

---
+++ counting_bits/Test.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Test {
+	public static void main(String[] args) {
+		Solution solver = new Solution();
+		int num = 5;
+		int[] ret = solver.countBits(num);
+		System.out.println(Arrays.toString(ret));
+	}
+}
